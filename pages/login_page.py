d7af91f6f3f12b41794fce057789bbf49789a25e
Takka1978 <talgmus@gmail.com>
2020-11-17 22:55:26 +0300

    В этой папке собраны все объекты проекта по системе PageObject

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 ﻿from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -15,5 +16,16 @@ class LoginPage(BasePage):
         
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
-      
+
+    def register_new_user(self, x, y):
+        input1 = self.browser.find_element(*LoginPageLocators.EMAIL_LINK)
+        input1.send_keys(x)
+        input2 = self.browser.find_element(*LoginPageLocators.PASSWORD_LINK1)
+        input2.send_keys(y)
+        input3 = self.browser.find_element(*LoginPageLocators.PASSWORD_LINK2)
+        input3.send_keys(y)
+        reg_btn = self.browser.find_element(*LoginPageLocators.REGIS_BTN)
+        reg_btn.click()
+
+
         
--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,10 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
-class MainPage(BasePage): 
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
+
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
